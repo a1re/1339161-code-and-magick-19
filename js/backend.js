978abc34fab35af945f46ada69f4799c9f1ec5c2
Yuriy <yuriy.marin@gmail.com>
2020-02-14 00:20:14 +0300

    Добавлены сохранение профиля мага и обработка ошибок
    В js/backend.js добавлена функция save, которая отправляет данные
    на сервер через XMLHttpRequest.
    
    В js/setup.js добавлен setupSubmitHandler, который вызывается
    по сабмиту и сохраняет данные через функцию backend.save, передавая
    ей коллбеки setupLoadHander() и setupLoadHander(), которые опредлены
    здесь же.
    
    В сss/main.css добавлены стили загрузки и ошибки. Статус выводится
    прямо в кнопку сохранения.

--- js/backend.js
+++ js/backend.js
@@ -2,7 +2,10 @@
 
 window.backend = (function () {
   var LOAD_URL = '//js.dump.academy/code-and-magick/data?callback=similarWizards';
+  var SAVE_URL = '//js.dump.academy/code-and-magick';
   var SIMILAR_WIZARDS_ERROR = 'Вы уникальный маг, похожих не нашлось';
+  var XHR_TIMEOUT_IN_MS = 10;
+  var SAVE_FORM_ERROR = 'Ошибка. Перезагрузите страницу';
 
   var load = function (loadHandler, errorHandler) {
     window.similarWizards = function (data) {
@@ -17,8 +20,38 @@ window.backend = (function () {
     document.body.append(loader);
   };
 
+  var save = function (data, loadHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case 200:
+          loadHandler();
+          break;
+
+        default:
+          errorHandler('Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      errorHandler(SAVE_FORM_ERROR);
+    });
+
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Таймаут ' + xhr.timeout + ' сек. Перезагрузите страницу');
+    });
+
+    xhr.timeout = XHR_TIMEOUT_IN_MS * 1000;
+
+    xhr.responseType = 'json';
+    xhr.open('POST', SAVE_URL);
+    xhr.send(data);
+  };
+
   return {
-    load: load
+    load: load,
+    save: save
   };
 
 })();
--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,8 @@
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var SAVE_BUTTON_LOADING = 'Сохранение...';
+  var SAVE_BUTTON_DEFAULT = 'Сохранить';
 
   var userNameInput = document.querySelector('.setup-user-name');
   var coatColorInput = document.querySelector('input[name=coat-color]');
@@ -12,6 +14,8 @@
   var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
   var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
   var fireball = document.querySelector('.setup-fireball-wrap');
+  var setupForm = document.querySelector('.setup-wizard-form');
+  var submitButton = document.querySelector('.setup-submit');
 
   var tossColorElement = function (colorElement, colorArray, colorInput) {
     var newColor = colorArray[Math.round((colorArray.length - 1) * Math.random())];
@@ -21,6 +25,25 @@
       colorElement.style.backgroundColor = newColor;
     }
     colorInput.value = newColor;
+  };
+
+  var setupSubmitHandler = function (evt) {
+    evt.preventDefault();
+    var data = new FormData(document.querySelector('.setup-wizard-form'));
+    submitButton.textContent = SAVE_BUTTON_LOADING;
+    submitButton.classList.add('loading');
+    window.backend.save(data, setupLoadHander, setupErrorHandler);
+  };
+
+  var setupLoadHander = function () {
+    submitButton.classList.remove('loading');
+    submitButton.textContent = SAVE_BUTTON_DEFAULT;
+    setupWindow.close();
+  };
+
+  var setupErrorHandler = function (message) {
+    submitButton.classList.add('error');
+    submitButton.textContent = message;
   };
 
   var setupWindow = window.dialog('.setup', 'hidden');
@@ -48,6 +71,8 @@
   fireball.addEventListener('click', function () {
     tossColorElement(fireball, FIREBALL_COLORS, fireballColorInput);
   });
+
+  setupForm.addEventListener('submit', setupSubmitHandler);
 
   window.setup = {
     COAT_COLORS: COAT_COLORS,
